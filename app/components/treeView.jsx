1554376c48488d064eb3d8d0d0787b0b6aa3e8c8
Alec Lownes <alec.lownes@gmail.com>
2016-11-29 13:17:07 -0500

    Simplify for bug example

--- app/components/treeView.jsx
+++ app/components/treeView.jsx
@@ -1,14 +1,26 @@
 import 'aframe';
 import {Scene, Entity} from 'aframe-react';
 import React from 'react';
-import {connect} from 'react-redux';
-import {prune, growTree} from '../actions';
+import Immutable from 'immutable';
 
 var TreeView = React.createClass({
+	getInitialState: function(){
+		return {
+			data: Immutable.fromJS({
+				base: [
+					'0,0,0'
+				],
+				blocks: {
+					'0,0,0': 'TRUNK',
+					'0,1,0': 'LEAF'
+				}
+			})
+		};
+	},
 	render: function(){
 		const blockScale = 0.1;
 		const userHeight = 1.6;
-		var [...blocks] = this.props.tree.get('blocks').entries();
+		var [...blocks] = this.state.data.get('blocks').entries();
 		var aBlocks = blocks.map((elem) => {
 			var key = elem[0];
 			var [x, y, z] = key.split(',');
@@ -24,13 +36,12 @@ var TreeView = React.createClass({
 					}}
 					material={'color: '+color}
 					position={[x*blockScale, y*blockScale+userHeight, z*blockScale]}
-					key={x+','+y+','+z}
-					onClick={()=>{this.props.prune(key)}}/>
+					key={x+','+y+','+z}/>
 			)
 		});
 		return (
 			<Scene
-				onClick={()=>{console.log('grow');this.props.growTree()}}>
+				onClick={()=>{console.log('grow');this.growTree()}}>
 				{aBlocks}
 				<Entity
 					position={[0.2, 0, 0.2]}
@@ -53,29 +64,67 @@ var TreeView = React.createClass({
 				</Entity>
 			</Scene>
 		);
+	},
+	growTree: function(){
+		var tree = this.state.data;
+		var options = {
+			growTrunk: true
+		};
+		var possibleTrunks = [];
+		var possibleLeaves = [];
+		var seen = new Set();
+		var next = tree.get('base').toJS();
+		while (next.length) {
+			let nextNext = [];
+			for (let i = 0; i < next.length; i++){
+				let key = next[i];
+				let type = tree.getIn(['blocks', key]);
+				let [x, y, z] = key.split(',');
+				x = parseInt(x);
+				y = parseInt(y);
+				z = parseInt(z);
+				let newKeys = [
+					(x+1)+','+y+','+z,
+					(x-1)+','+y+','+z,
+					x+','+(y+1)+','+z,
+					x+','+(y-1)+','+z,
+					x+','+y+','+(z+1),
+					x+','+y+','+(z-1)
+				];
+				for (let k = 0; k < newKeys.length; k++){
+					let newKey = newKeys[k];
+					let newType = tree.getIn(['blocks', newKey]);
+					let newY = parseInt(newKey.split(',')[1]);
+					if (!seen.has(newKey)){
+						if (tree.hasIn(['blocks', newKey])){
+							nextNext.push(newKey);
+							if (newType == 'LEAF' && type == 'TRUNK' && newY >= 0){
+								//trunk grow into leaf
+								possibleTrunks.push(newKey);
+							}
+						}
+						else if (type == 'TRUNK' && newY >= 0){
+							//Trunk grow into air
+							possibleTrunks.push(newKey);
+						}
+					}
+				}
+				seen.add(key);
+			}
+			next = nextNext;
+		}
+		if (options.growTrunk){
+			var selected = possibleTrunks[Math.floor(Math.random()*possibleTrunks.length)];
+			tree = tree.set('blocks', tree.get('blocks').set(selected, 'TRUNK'));
+			if (selected.split(',')[1] == '0'){
+				//add to base
+				tree = tree.set('base', tree.get('base').push(selected));
+			}
+		}
+		this.setState({
+			data: tree
+		});
 	}
 });
 
-var mapStateToProps = (state) => {
-	return {
-		tree: state
-	}
-};
-
-var mapDispatchToProps = (dispatch) => {
-	return {
-		prune: function(key){
-			dispatch(prune(key));
-		},
-		growTree: function(){
-			dispatch(growTree({growTrunk: true}))
-		}
-	};
-}
-
-export default connect(
-	mapStateToProps,
-	mapDispatchToProps
-)(
-	TreeView
-);
+export default TreeView;
--- index.js
+++ index.js
@@ -7,23 +7,14 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
 const http = require('http').Server(app);
-const Sequelize = require('sequelize');
-const db = new Sequelize(process.env.DATABASE_URL, {
-  logging: false
-});
-
-const models = db.import(__dirname + '/models');
 
 //sync all sequelize models
-db.sync();
 
 //parse application/json
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cookieParser());
-
-app.use(session({secret: process.env.DATABASE_URL}));
 
 //Deliver the public folder statically
 app.use(express.static('public'));
@@ -32,19 +23,10 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 //This tells the server to listen
-var port = process.env.PORT;
+var port = 4000;
 http.listen(port, function(){
   console.log('Example app listening on port '+port+'!');
 });
-
-//This is the options object that will be passed to the api files
-let apiOptions = {
-  app: app,
-  models: models
-};
-
-//Load the api versions
-require('./api/v1')(apiOptions);
 
 /*
 * This tells the server to always serve index.html no matter what,
